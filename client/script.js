71c6cd47206e3c6aef669b6ab4a66a67e5f8573c
riynare <vorobeykirillwork@gmail.com>
2026-05-24 01:09:43 +0300

    Добавил css

--- client/script.js
+++ client/script.js
@@ -1,8 +1,7 @@
 const grid = document.getElementById("grid");
-const size = 20;
+const size = +prompt("Введите размер окна(NxN)");
 for (let i = 0; i < size ** 2; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.textContent = "";
-    grid.appendChild(cell);
+    grid.append(cell);
 }
