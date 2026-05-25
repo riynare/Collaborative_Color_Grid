4e777903b857de04967b6ab17c8eed398b5b1e0b
riynare <vorobeykirillwork@gmail.com>
2026-05-25 17:14:06 +0300

    Сделал адаптивную верстку

--- client/script.js
+++ client/script.js
@@ -1,5 +1,7 @@
 const grid = document.getElementById("grid");
 const size = +prompt("Введите размер окна(NxN)");
+grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 for (let i = 0; i < size ** 2; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
