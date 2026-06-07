d91cc449b5d725101adaa0f4a3e9d254a2a12f76
riynare <vorobeykirillwork@gmail.com>
2026-06-07 20:51:27 +0300

    Arrows

--- client/color-picker.js
+++ client/color-picker.js
@@ -1,13 +1,25 @@
 const colorMenu = document.getElementById("colorMenu");
-colorMenu.addEventListener("click", (event) => {
-    const color = event.target.dataset.color;
-    if (!color) return;
-    if (!window.selectedCell) {
-        alert("Сначала выбери клетку");
-        return;
-    }
-    window.selectedCell.style.backgroundColor = color;
-    window.selectedCell.style.border = "1px solid gray";
+function hideColorMenu() {
     colorMenu.classList.remove("unhidden");
     colorMenu.classList.add("hidden");
-})
+}
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        hideColorMenu();
+        window.selectedCell.style.border = "1px solid gray";
+        window.selectedCell.blur();
+    }
+});
+document.addEventListener("click", (event) => {
+    if (!colorMenu.contains(event.target)) {
+        hideColorMenu();
+        window.selectedCell.style.border = "1px solid gray";
+    }
+});
+colorMenu.addEventListener("click", (event) => {
+    event.stopPropagation();
+    const color = event.target.dataset.color;
+    window.selectedCell.style.backgroundColor = color;
+    window.selectedCell.style.border = "1px solid gray";
+    hideColorMenu();
+});                    
--- client/script.js
+++ client/script.js
@@ -1,21 +1,84 @@
 window.selectedCell = null;
 const grid = document.getElementById("grid");
 const size = +prompt("Введите размер окна(NxN)");
+const cellArray = [];
 grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+function openMenu(cell, x, y) {
+    if (window.selectedCell) {
+        window.selectedCell.style.border = "1px solid gray";
+    }
+    window.selectedCell = cell;
+    cell.style.border = "2px solid black";
+
+    colorMenu.classList.remove("hidden");
+    colorMenu.classList.add("unhidden");
+
+    colorMenu.style.position = "fixed";
+    colorMenu.style.left = `${x}px`;
+    colorMenu.style.top = `${y}px`;
+}
 for (let i = 0; i < size ** 2; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.addEventListener("click", (e) => {
-        if (window.selectedCell) {
-            window.selectedCell.style.border = "1px solid gray";
+    cellArray.push(cell);
+    cell.tabIndex = 0;
+    cell.addEventListener("click", (event) => {
+        event.stopPropagation();
+        const rect = cell.getBoundingClientRect();
+        openMenu(cell, rect.right, rect.top);
+    });
+    cell.addEventListener("keydown", (event) => {
+        let currentIndex = cellArray.indexOf(event.currentTarget);
+        let newIndex = currentIndex;
+
+        switch (event.key) {
+            case "ArrowUp":
+                newIndex = currentIndex - size;
+                break;
+
+            case "ArrowDown":
+                newIndex = currentIndex + size;
+                break;
+
+            case "ArrowRight":
+                if (currentIndex % size !== size - 1) {
+                    newIndex = currentIndex + 1;
+                }
+                break;
+
+            case "ArrowLeft":
+                if (currentIndex % size !== 0) {
+                    newIndex = currentIndex - 1;
+                }
+                break;
+
+            default:
+                return;
         }
-        window.selectedCell = cell;
-        window.selectedCell.style.border = "2px solid black";
-        colorMenu.classList.remove("hidden");
-        colorMenu.classList.add("unhidden");
-        colorMenu.style.left = e.pageX + "px";
-        colorMenu.style.top = e.pageY + "px";
+
+        event.preventDefault();
+
+        if (newIndex >= 0 && newIndex < cellArray.length) {
+            const newCell = cellArray[newIndex];
+            newCell.focus();
+            colorMenu.classList.remove("unhidden");
+            colorMenu.classList.add("hidden");
+            if (window.selectedCell) {
+            window.selectedCell.style.border = "1px solid gray";
+            }
+            newCell.addEventListener("keydown", (event) => {
+                if (event.key === "Escape") {
+                    newCell.blur();
+                }
+            });
+            newCell.addEventListener("keydown", (event) => {
+                if (event.key === "Enter" || event.key === " ") {
+                    const rect = newCell.getBoundingClientRect();
+                    openMenu(newCell, rect.right, rect.top);
+                }
+            })
+        }
     });
     grid.append(cell);
-}
+};
